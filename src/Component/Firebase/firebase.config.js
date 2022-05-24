45ee4c606e0d9f9036faa4cd3d33fc39f7b4a7fa
rashedsabbir <rashedulhasan1996@gmail.com>
2022-05-24 20:14:05 +0600

    firebase api added

---
+++ src/Component/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDyuv1XTQo_1ua-sZEGMIgYxVE890zwLC0",
+  authDomain: "chunk-manufacturer.firebaseapp.com",
+  projectId: "chunk-manufacturer",
+  storageBucket: "chunk-manufacturer.appspot.com",
+  messagingSenderId: "1014174139626",
+  appId: "1:1014174139626:web:d9fe3beeca6d1433e64393",
+  };
+  export default firebaseConfig
